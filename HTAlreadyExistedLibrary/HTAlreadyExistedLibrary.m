65e52f9fe1313bbdd59206a5a9cffa7987d14988
chengxuyuanxiaoka <1246901487@qq.com>
2020-04-28 13:28:18 +0800

    second
    Signed-off-by: chengxuyuanxiaoka <1246901487@qq.com>

--- HTAlreadyExistedLibrary/HTAlreadyExistedLibrary.m
+++ HTAlreadyExistedLibrary/HTAlreadyExistedLibrary.m
@@ -8,11 +8,40 @@
 
 #import "HTAlreadyExistedLibrary.h"
 #import "HTShareManager.h"
+#import <UIKit/UIKit.h>
+#import "HTTestController.h"
 
 @implementation HTAlreadyExistedLibrary
 
 - (void)setupIntroduceInfo:(UserInfo *)userInfo resultBlock:(void (^)(NSDictionary * resultDict))introduceResultBlock {
     [HTShareManager sharedManager].htResultBlock = introduceResultBlock;
+    HTTestController *testVC = [[HTTestController alloc] init];
+    [[self getCurrentVC] presentViewController:testVC animated:YES completion:nil];
+}
+
+//获取当前屏幕显示的viewcontroller
+- (UIViewController *)getCurrentVC {
+    UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+    UIViewController *currentVC = [self getCurrentVCFrom:rootViewController];
+    return currentVC;
+}
+
+- (UIViewController *)getCurrentVCFrom:(UIViewController *)rootVC {
+    UIViewController *currentVC;
+    if ([rootVC presentedViewController]) {
+        // 视图是被presented出来的
+        rootVC = [rootVC presentedViewController];
+    }
+    if ([rootVC isKindOfClass:[UITabBarController class]]) {
+        currentVC = [self getCurrentVCFrom:[(UITabBarController *)rootVC selectedViewController]];
+    } else if ([rootVC isKindOfClass:[UINavigationController class]]){
+        currentVC = [self getCurrentVCFrom:[(UINavigationController *)rootVC visibleViewController]];
+    } else {
+        // 根视图为非导航类
+        currentVC = rootVC;
+    }
+    
+    return currentVC;
 }
 
 @end
